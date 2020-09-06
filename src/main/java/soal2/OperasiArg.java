1150b189def4ca72200f4993d76846476ce2d2e6
intechristina <70269081+intechristina@users.noreply.github.com>
2020-09-06 15:35:27 +0700

    Update OperasiArg.java

--- src/main/java/soal2/OperasiArg.java
+++ src/main/java/soal2/OperasiArg.java
@@ -5,7 +5,7 @@ public class OperasiArg{
       public static void main (String args[]){
 			int a,b;
 			 a=Integer.parseInt(args[0]);
-			 b=Integer.parseInt(args[1]);
+			 b=args[0];
 			 System.out.print("" + a + "x" + b + "=" + kali(a,b)+"\n");
 			 System.out.print("" + a + "/" + b + "=" + bagi(a,b)+"\n");
 			 System.out.print("" + a + "+" + b + "=" + tambah(a,b)+"\n");
